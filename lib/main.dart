b8d76c6d38ca2533397035589a273d878cca448b
DELL <bsem-f18-056@superior.edu.pk>
2022-02-16 17:03:47 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:client_screen/Therapist_side_app/Bottom_navigation_bar/Bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'bottom_bar/bottom_navigation.dart';
-import 'bottom_bar/custom_navigation_bar.dart';
+import 'Client_side_application/bottom_bar/custom_navigation_bar.dart';
+
 List<CameraDescription>? cameras;
 void main() async{
 
@@ -23,19 +24,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-      //  visualDensity: VisualDensity.adaptivePlatformDensity,
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+     
         primarySwatch: Colors.blue,
       ),
-      home:   Navigatiobar(),
+      home:    Bottom_navigation(),
+            // Navigatiobar(),    for client side app
     );
   }
 }
